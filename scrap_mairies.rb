568e8e168c255e8929e46d6563dcdb06ad3d1998
Fredis <fredericstojicevic@gmail.com>
2018-10-11 15:16:19 +0200

    The CEO can directly get the townhalls with the respective email addresses.

--- scrap_mairies.rb
+++ scrap_mairies.rb
@@ -2,11 +2,15 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
+$array_with_results = []
+
 #Méthode pour récupérer l'adresse email d'une mairie à partir de sa page
 def get_the_email_of_a_townhall_from_its_webpage(page_url)
+	
 	data = Nokogiri::HTML(open(page_url))
 	data.xpath("/html/body/div[1]/main/section[2]/div/table/tbody/tr[4]/td[2]").each do |node|
 		puts node.text
+		return node.text
 	end
 end
 
@@ -17,16 +21,19 @@ def get_all_the_urls_of_val_doise_townhalls
 	#get_townhalls_URL.css('a.lientxt').each do |url|
 	#	puts url['href']
 	array_links = []
-	get_townhalls_URL.xpath('//a[@class = "lientxt"]').each do |url| #solution avec XPATH autrement au-dessus en commentaires, la solution avec CSS
+	get_townhalls_URL.xpath('//a[@class = "lientxt"]').each do |url| #Solution avec XPATH autrement au-dessus en commentaires, la solution avec CSS
 		link = url['href']
-		link[0] = '' #on enlève les points sur les URLs récupérées
-		link = "http://annuaire-des-mairies.com#{link}" #on formatte au bon format d'URL
-		array_links << link #on ajoute nos liens dans un tableau
+		link[0] = '' #On enlève les points sur les URLs récupérées
+		link = "http://annuaire-des-mairies.com#{link}" #On formatte au bon format d'URL
+		hash_townhall_info = {:name => url.text, :townhall_mail => link} #Création de hash
+		array_links << hash_townhall_info #Ajout du hash dans un tableau
 	end
-	
-	array_links.each do |url|
-		get_the_email_of_a_townhall_from_its_webpage(url) #on lance la première méthode afin de récupérer les adresses emails par page de mairie
-	end
+
+
+	array_links.each do |hash_townhall|
+		hash_townhall[:townhall_mail] = get_the_email_of_a_townhall_from_its_webpage(hash_townhall[:townhall_mail])
+	end	
+	puts array_links
 
 end
 
